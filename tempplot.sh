0fa7dfe139eae28d36e1020c59eceb418ee027dd
Gabriele Santambrogio <santambrogio@lens.unifi.it>
2025-02-06 09:43:18 +0100

    add timestamp to gnuplot script

--- tempplot.sh
+++ tempplot.sh
@@ -11,6 +11,7 @@ set ytics nomirror
 set xlabel 'Time'
 set ylabel 'Temp (K)'
 set y2label 'mbar'
+set timestamp
 plot '$1' u 1:2 w l axis x1y1 tit 'T1', '$1' u 1:3 w l axis x1y1 tit 'T2','$1' u 1:4 w l axis x1y1 tit 'T3','$1' u 1:5 w l axis x1y1 tit 'T4', '$1' u 1:6 w l axis x1y2 tit 'Pirani',  '$1' u 1:7 w l axis x1y2 tit 'Cold cathode gauge' lt -1
 while(1){
 	pause 10
